a9efa9fcc5098a4fdb3f6b3cace627c7f3218a8a
deepkarsarkar <dksarkar1211@gmail.com>
2025-12-30 10:20:16 +0530

    First functional commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,8 @@
+export const contactsData = {
+    email: 'deep.ksarkar1211@gmail.com',
+    phone: '(+91) 8420057039',
+    address1: 'Bengaluru, Karnataka, India - 560102 ',
+    address2: 'Chandannagar, Hooghly, West Bengal, India - 712136',
+
+    sheetAPI: ''
+}
---
+++ src/data/experienceData.js
@@ -0,0 +1,44 @@
+export const experienceData = [
+    // {
+    //     id: 1,
+    //     company: 'Globex Corporation',
+    //     jobtitle: 'Frontend Developer',
+    //     startYear: '2018',
+    //     endYear: '2019'
+    // },
+    // {
+    //     id: 2,
+    //     company: 'Vehement Capital Partners',
+    //     jobtitle: 'Backend Developer',
+    //     startYear: '2019',
+    //     endYear: '2020'
+    // },
+    // {
+    //     id: 3,
+    //     company: 'Wonka Industries',
+    //     jobtitle: 'Marketing',
+    //     startYear: '2020',
+    //     endYear: 'Present'
+    // },
+    {
+        id: 1,
+        company: 'PravegaSemi Pvt. Ltd.',
+        jobtitle: 'VLSI Design Engineer',
+        startYear: 'Feb 2025',
+        endYear: 'Present'
+    },
+    {
+        id: 2,
+        company: 'PricewaterhouseCoopers Services India LLP',
+        jobtitle: 'Technology Consultant',
+        startYear: 'Jul 2022',
+        endYear: 'Jan 2025'
+    },
+    {
+        id: 3,
+        company: 'Tata Consultancy Services Ltd.',
+        jobtitle: 'Assistant System Engineer',
+        startYear: 'Nov 2018',
+        endYear: 'Sep 2020'
+    },
+]
